ba3a780ce27ace128f96c3244c8fe3f73d20a800
Nayan Das <ndas5144@gmail.com>
2023-04-18 23:12:49 +0530

    removed heroku dependencies from settings

--- remotebirds/settings.py
+++ remotebirds/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,4 +155,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os .path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, ' static'), )
-django_heroku.settings(locals())
